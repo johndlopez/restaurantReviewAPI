820d8eda98db1d1da132853350a25ee1eb92da72
johndlopez <johnlopez@Johns-MacBook-Pro-2.local>
2018-11-27 11:07:31 -0800

    added login routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const reviewController = require('../controllers/reviewController');
+const authController = require('../controllers/authController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/reviews', catchErrors(reviewController.getReviews));
 router.post('/review/create', catchErrors(reviewController.createReview));
+
+router.post('/reviews/:id/edit', catchErrors(reviewController.editReview));
+
+router.post('/login', authController.login);
 
 module.exports = router;

ce8b5378c262976eba5c16929e405dd056ca26bf
johndlopez <johnlopez@Johns-MacBook-Pro-2.local>
2018-11-27 13:16:03 -0800

    added registration path

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const reviewController = require('../controllers/reviewController');
 const authController = require('../controllers/authController');
+const userController = require('../controllers/userController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/reviews', catchErrors(reviewController.getReviews));
@@ -10,5 +11,11 @@ router.post('/review/create', catchErrors(reviewController.createReview));
 router.post('/reviews/:id/edit', catchErrors(reviewController.editReview));
 
 router.post('/login', authController.login);
+
+router.post(
+  '/register',
+  userController.validateRegister,
+  catchErrors(userController.register)
+);
 
 module.exports = router;

5e211d441f6f1a687d8699f897780b55d519fb8a
johndlopez <johnlopez@Johns-MacBook-Pro-2.local>
2018-11-27 13:15:50 -0800

    added registration

---
+++ controllers/userController.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+const { promisify } = require('es6-promisify');
+
+exports.validateRegister = (req, res, next) => {
+  req.sanitizeBody('name');
+  req.checkBody('name', 'You must supply a name!').notEmpty();
+  req.checkBody('email', 'That Email is not valid!').isEmail();
+  req.sanitizeBody('email').normalizeEmail({
+    gmail_remove_dots: false,
+    remove_extension: false,
+    gmail_remove_subaddress: false
+  });
+  req.checkBody('password', 'Password Cannot be Blank!').notEmpty();
+  req
+    .checkBody('password-confirm', 'Confirmed Password cannot be blank!')
+    .notEmpty();
+  req
+    .checkBody('password-confirm', 'Oops! Your passwords do not match')
+    .equals(req.body.password);
+
+  const errors = req.validationErrors();
+  if (errors) {
+    errors.map(err => console.log(err.msg));
+    res.send(req.body);
+    return;
+  }
+  next(); //pass to register
+};
+
+exports.register = async (req, res, next) => {
+  const user = new User({ email: req.body.email, name: req.body.name });
+  const register = await User.register(user, req.body.password);
+  res.send(register.email);
+  next(); // pass to authController.login
+};

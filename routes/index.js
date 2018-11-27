ed2e03488375a660e5daa94b566862f955bbf9e7
johndlopez <johnlopez@Johns-MacBook-Pro-2.local>
2018-11-27 10:30:34 -0800

    added create review route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const reviewController = require('../controllers/reviewController');
+const { catchErrors } = require('../handlers/errorHandlers');
 
-router.get('/reviews', reviewController.getReviews);
+router.get('/reviews', catchErrors(reviewController.getReviews));
+router.post('/review/create', catchErrors(reviewController.createReview));
 
 module.exports = router;
